8188fdecbe4d2f8ea7b1e3cfb5eb18785a493f0f
Zigludo123 <56159408+Zigludo123@users.noreply.github.com>
2020-09-09 16:00:26 +0200

    Implementation of sorting by suit and by value

--- src/cardlogic/SetOfCards.java
+++ src/cardlogic/SetOfCards.java
@@ -2,10 +2,13 @@ package cardlogic;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+
+import cardlogic.Card.SUITS;
 
 public class SetOfCards {
 	
-	ArrayList<Card> cards;
+	List<Card> cards;
 	
 	
 	SetOfCards(boolean deck){
@@ -37,6 +40,35 @@ public class SetOfCards {
 	
 	void addCard(Card card) {
 		this.cards.add(card);
+	}
+	
+	void sortBySuits() {
+		
+		List<Card> resList = new ArrayList<>();
+		
+		for(SUITS aSuit : SUITS.values()) {
+			List<Card> tempList = new ArrayList<>();
+			
+			for(Card aCard : this.cards) {
+				if (aCard.getSuit() == aSuit) {
+					tempList.add(aCard);
+					this.cards.remove(aCard);
+				}
+				
+			}
+			
+			Collections.sort(tempList);	
+			resList.addAll(tempList);
+			
+		}
+		
+		this.cards = resList;
+		
+		
+	}
+	
+	void sortByValue() {
+		Collections.sort(this.cards);
 	}
 	
 	void shuffleCards() {
