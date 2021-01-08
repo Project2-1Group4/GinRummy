9c1b174a5a50b5807062a37d90c7893ecebf44df
EmeryK-1 <emery1411@gmail.com>
2021-01-08 12:19:24 +0100

    Test indexing slip up

--- core/src/temp/Extra/Tests.java
+++ core/src/temp/Extra/Tests.java
@@ -29,7 +29,7 @@ public class Tests {
                 new basicGreedyTest(),
                 new basicGreedyTest(),
         };
-        int games = 5000; // Set nb of games
+        int games = 1000; // Set nb of games
         Integer seed = 5; // Set seed
 
         List<GameState> results = runGames(players, games, seed);
@@ -39,6 +39,9 @@ public class Tests {
             wins[result.getHighestScoreIndex()]++;
         }
         System.out.println(Arrays.toString(wins)+" wins out of "+games);
+        for (int i = 0; i < players.length; i++) {
+            System.out.println("Player "+i+" win%: "+(wins[i]/(double)games));
+        }
         //CSVWriterV2.write(results, "Results/Delete this/","n");
     }
 
